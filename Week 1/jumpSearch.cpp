61ce50e15a8deb464c171e3f624d7b7e08a37175
adityarawat1337x <rkrawat98766@gmail.com>
2021-07-29 02:16:25 +0530

    linear search

---
+++ Week 1/jumpSearch.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void jump(vector<int>& arr,int key)
+{
+    int n = arr.size();
+    int step = sqrt(n);
+    int start = 0,end = step,comp=0;
+    while(++comp && arr[end]<=key && end<n)
+    {
+        start = end;
+        end+=step;
+        if(end>n-1)
+            end = n;
+    }
+    for(int i=start ;i<end;i++)
+    {
+        if(++comp && arr[i]==key)
+        {
+            cout << key << " is present\n";
+            cout << "Total comparisons = " << comp;
+            return;
+        }
+    }
+    cout<<key <<" is not present\n";
+    cout<<"Total comparisons = "<<comp;
+
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,key;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cin>>key;
+        jump(arr,key);
+    }
+}
--- Week 1/problem!.cpp
+++
@@ -1,9 +0,0 @@
-
-/*
- * Given an already shorted array of positive numbers, design an algorithm and
- * implement it using a program to find whether given key element is presnet in
- * the array or not. Also, find the total number of comparsions for each input
- * case/ Time complexity = O(n*log(n)), where n is the size of input).
- */
-
-
